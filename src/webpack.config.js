0d85310dee0a19dd61cffead41c43c7a395bdbd1
John Abiola <rtmabiola@gmail.com>
2023-09-17 07:02:00 +0100

    Update webpack.config.js to fix testing

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
-const crypto = require("crypto");
+const path = require('path');
+const crypto = require('crypto');
 const crypto_orig_createHash = crypto.createHash;
-crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+crypto.createHash = algorithm => cryptoOrigCreateHash(algorithm === 'md4' ? 'sha256' : algorithm);
 
 module.exports = {
   entry: {
